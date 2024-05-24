ddc7eb7273e3f314b6e194780039e980f63400b7
Class_Student <321@mail.ru>
2024-05-24 15:33:34 +0300

    rar

---
+++ app/src/main/java/com/example/sec/OnBoardingActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sec
+
+import LoginActivity
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+
+
+class OnBoardingActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+    fun GoToSecAct(v: View?) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
