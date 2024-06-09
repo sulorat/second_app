fb180528fa4ee19d1a2c5a844279d9e85571e541
sulorat <dima.bessarab@list.ru>
2024-06-10 00:58:47 +0300

    r

--- app/src/main/java/com/example/sec/MainActivity.kt
+++ app/src/main/java/com/example/sec/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.sec
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import java.util.logging.Handler
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splashscreen)
 
         android.os.Handler().postDelayed({
             val intent = Intent(this, OnBoardingActivity::class.java)
